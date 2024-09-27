21c09d4ccfe343d362ee9e6e2742ebad3d7f7024
pangyuteng <pangyuteng@gmail.com>
2024-09-27 15:51:28 -0700

    wip

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 docker build -t pangyuteng/drr:latest .
 docker push pangyuteng/drr:latest 
---
+++ dicomdir.py
@@ -0,0 +1,28 @@
+import sys
+import pathlib
+import pydicom
+import pandas as pd
+
+folder = sys.argv[1]
+csv_file = sys.argv[2]
+file_list =  [str(x) for x in pathlib.Path(folder).rglob("*") if not x.is_dir() and pydicom.misc.is_dicom(str(x))]
+
+mylist = []
+for x in file_list:
+    ds = pydicom.dcmread(x,stop_before_pixels=True)
+    mylist.append(dict(
+        FilePath=x,
+        StudyInstanceUID=ds.StudyInstanceUID,
+        SeriesInstanceUID=ds.SeriesInstanceUID,
+        StudyDescription=ds.StudyDescription,
+        SeriesDescription=ds.SeriesDescription,
+    ))
+df = pd.DataFrame(mylist)
+df.to_csv(csv_file,index=False)
+
+
+"""
+docker run -it -v $PWD:/workdir pangyuteng/dcm:latest bash
+python dicomdir.py tmp/dcm_folder ok.csv
+
+"""
