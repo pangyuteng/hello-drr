d711c5715c2ffe0a48f04cd07c0b198b92692940
pangyuteng <pangyuteng@gmail.com>
2024-09-27 17:56:54 -0700

    update resolution

--- gen_drr.py
+++ gen_drr.py
@@ -35,8 +35,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 drr = DRR(
     subject,     # An object storing the CT volume, origin, and voxel spacing
     sdd=1020.0,  # Source-to-detector distance (i.e., focal length)
-    height=200,  # Image height (if width is not provided, the generated DRR is square)
-    delx=2.0,    # Pixel spacing (in mm)
+    height=2000,  # Image height (if width is not provided, the generated DRR is square)
+    delx=0.2,    # Pixel spacing (in mm)
 ).to(device)
 
 # Set the camera pose with rotations (yaw, pitch, roll) and translations (x, y, z)
