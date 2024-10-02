907b089eb3f4c8797cdee50e25f1c6fd3176e639
pangyuteng <pangyuteng@gmail.com>
2024-10-02 10:14:25 -0700

    testing saving of masks

--- gen_drr_with_mask.py
+++ gen_drr_with_mask.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import numpy as np
-import pandas as pd 
+import pandas as pd
+import imageio
 import SimpleITK as sitk
 import uuid
 
@@ -12,47 +13,49 @@ from diffdrr.drr import DRR
 from diffdrr.data import read
 from diffdrr.visualization import plot_drr, plot_mask
 
-image_nifti_file = sys.argv[1]
-mask_nifti_file = sys.argv[2]
-png_file = sys.argv[3]
-device_id = sys.argv[4] # `cpu` or `gpu` or `cuda:7`
+def main(image_nifti_file,mask_nifti_file,png_file,device_id):
+    subject = read(image_nifti_file,mask_nifti_file)
+    print(subject.shape)
 
-subject = read(image_nifti_file,mask_nifti_file)
-print(subject.shape)
+    # Initialize the DRR module for generating synthetic X-rays
+    device = torch.device(device_id)
 
-# Initialize the DRR module for generating synthetic X-rays
-device = torch.device(device_id)
+    drr = DRR(
+        subject,     # An object storing the CT volume, origin, and voxel spacing
+        sdd=1020.0,  # Source-to-detector distance (i.e., focal length)
+        height=1024,  # Image height (if width is not provided, the generated DRR is square)
+        width=1024,
+        delx=0.4,    # Pixel spacing (in mm)
+        renderer='siddon', # 'siddon' or 'trilinear'
+    ).to(device)
 
-drr = DRR(
-    subject,     # An object storing the CT volume, origin, and voxel spacing
-    sdd=1020.0,  # Source-to-detector distance (i.e., focal length)
-    height=512,  # Image height (if width is not provided, the generated DRR is square)
-    delx=0.8,    # Pixel spacing (in mm)
-).to(device)
+    # Set the camera pose with rotations (yaw, pitch, roll) and translations (x, y, z)
+    rotations = torch.tensor([[0.0, 0.0, 0.0]], device=device)
+    translations = torch.tensor([[0.0, 850.0, 0.0]], device=device)
 
-# Set the camera pose with rotations (yaw, pitch, roll) and translations (x, y, z)
-rotations = torch.tensor([[0.0, 0.0, 0.0]], device=device)
-translations = torch.tensor([[0.0, 850.0, 0.0]], device=device)
+    # 📸 Also note that DiffDRR can take many representations of SO(3) 📸
+    # For example, quaternions, rotation matrix, axis-angle, etc...
+    img = drr(rotations, translations, parameterization="euler_angles", convention="ZXY",
+        mask_to_channels=True)
 
-# 📸 Also note that DiffDRR can take many representations of SO(3) 📸
-# For example, quaternions, rotation matrix, axis-angle, etc...
-img = drr(rotations, translations, parameterization="euler_angles", convention="ZXY",
-    mask_to_channels=True)
+    # https://github.com/eigenvivek/DiffDRR/blob/5430fa67d5599e1071806a97bed17d38c633e092/notebooks/tutorials/introduction.ipynb
 
-# https://github.com/eigenvivek/DiffDRR/blob/5430fa67d5599e1071806a97bed17d38c633e092/notebooks/tutorials/introduction.ipynb
+    img = img.detach().cpu().numpy()
+    img = np.moveaxis(img.squeeze(),0,-1)
+    mydict = {'tmp/image.png':0,'tmp/mask1.png':1,'tmp/mask2.png':2}
+    for png_file,idx in mydict.items():
+        slice_img = img[:,:,idx]
+        min_val, max_val = np.min(slice_img),np.max(slice_img)
+        slice_img = 255*((slice_img-min_val)/(max_val-min_val)).clip(0,1)
+        slice_img = slice_img.astype(np.uint8)
+        imageio.imwrite(png_file, slice_img)
 
-print(img.shape)
-#sys.exit(1)
-#plot_drr(img, ticks=False)
-#plot_mask(img)
-img = img.detach().cpu().numpy()
-img = np.moveaxis(img.squeeze(),0,-1)
-for idx in range(3):
-    plt.subplot(131+idx)
-    plt.imshow(img[:,:,idx],cmap='gray')
-    plt.tight_layout()
-    plt.show()
-plt.savefig(png_file)
+if __name__ == "__main__":
+    image_nifti_file = sys.argv[1]
+    mask_nifti_file = sys.argv[2]
+    png_file = sys.argv[3]
+    device_id = sys.argv[4] # `cpu` or `gpu` or `cuda:7`
+    main(image_nifti_file,mask_nifti_file,png_file,device_id)
 
 """
 docker run -it -v $PWD:/workdir pangyuteng/drr:latest bash
@@ -63,5 +66,5 @@ python3 gen_drr_with_mask.py \
     tmp/p19-1.3.12.2.1107.5.1.4.73191.30000018092214593450000003275-mask.nii.gz \
     tmp/test.png cpu
 
-
+'tmp/p19-1.3.12.2.1107.5.1.4.73191.30000018092214593450000003275-image.nii.gz','tmp/p19-1.3.12.2.1107.5.1.4.73191.30000018092214593450000003275-mask.nii.gz','tmp/test.png','cpu'
 """
